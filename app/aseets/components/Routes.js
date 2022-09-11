99e277c4bc44a9bb1cd669bb2894c543dcc58461
Agusibam <103153659+Agusibam@users.noreply.github.com>
2022-09-11 00:24:24 -0300

    Update Routes.js

--- app/aseets/components/Routes.js
+++ app/aseets/components/Routes.js
@@ -1,8 +1,8 @@
-import { Home } from "/app/aseets/components/Home.js";
-import { Techs } from "/app/aseets/components/Techs.js";
-import { Proyects } from "/app/aseets/components/Proyects.js";
-import { Contact } from "/app/aseets/components/contact.js";
-import { AboutMe } from "/app/aseets/components/AboutMe.js";
+import { Home } from "./aseets/components/Home.js";
+import { Techs } from "./aseets/components/Techs.js";
+import { Proyects } from "./aseets/components/Proyects.js";
+import { Contact } from "./aseets/components/contact.js";
+import { AboutMe } from "./aseets/components/AboutMe.js";
 
 
 const Router = (route) =>{
